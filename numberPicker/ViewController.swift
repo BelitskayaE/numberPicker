9fb51b5014f4a0cedb1e0fbf15e7c7742985bef9
Lisa9999 <lisomrr@gmail.com>
2020-09-24 17:56:08 +0300

    -make a photo; -send req to our

--- numberPicker/ViewController.swift
+++ numberPicker/ViewController.swift
@@ -8,12 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+       
+    
+    //make a photo
+    var imagePicker: UIImagePickerController!
+    @IBOutlet weak var ImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func ButtonClicked(_ sender: Any) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true,completion: nil)
+        ImageView.image = info[.editedImage] as? UIImage
+        sendRequest(image:ImageView.image!)
+    }
+    
+    //send request to ocr
+    let resourceString = "https://westus.api.cognitive.microsoft.com/vision/v2.0/ocr?"
+    
+    let parameters: [String : String] = [
+               "language": "en",
+               "detectOrientation": "true",
+           ]
+
+    
+    func sendRequest(image:UIImage) {
+        let resourceStringWithParams = resourceString + parameters["language"]! + parameters["detectOrientation"]!
+        let resourceUrl = URL(string: resourceStringWithParams)
+        var request = URLRequest(url: resourceUrl!)
+        request.httpMethod = "POST"
+        let requestBody = image.jpegData(compressionQuality: 100)
+        request.httpBody = requestBody
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                // Check for Error
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+         
+                // Convert HTTP Response Data to a String
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n \(dataString)")
+                }
+        }
+        task.resume()
+        
+    }
+    
+
+    
+    
 
 
 }
